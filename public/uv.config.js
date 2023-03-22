cfc64d3a63623893e8b2d2f4a916e663ed611d42
DEEZ Nuts <93239187+TheFalloutOf76@users.noreply.github.com>
2023-03-22 00:14:58 -0700

    Create uv.config.js

---
+++ public/uv.config.js
@@ -0,0 +1,13 @@
+// This file overwrites the stock UV config.js
+
+self.__uv$config = {
+  prefix: "/application/v2/",
+  bare: "/api/",
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: "/application/uv.handler.js",
+  client: "/application/uv.client.js",
+  bundle: "/application/uv.bundle.js",
+  config: "/application/uv.config.js",
+  sw: "/application/uv.sw.js",
+};

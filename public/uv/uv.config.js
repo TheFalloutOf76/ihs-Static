b55f40ce2377137b781c6a1013683b488c09db4c
DEEZ Nuts <93239187+TheFalloutOf76@users.noreply.github.com>
2023-03-21 18:47:06 -0700

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,7 +1,7 @@
 // This file overwrites the stock UV config.js
 
 self.__uv$config = {
-  prefix: "/application/v2",
+  prefix: "/application/v2/",
   bare: "/bare/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
